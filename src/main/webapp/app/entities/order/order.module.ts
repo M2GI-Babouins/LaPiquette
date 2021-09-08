29dd851a6f884456f9ed98dba330ae5d1e563c30
Morgane <morgane.henrion38@gmail.com>
2021-09-08 17:09:35 +0200

    kinda displaying the cart

--- src/main/webapp/app/entities/order/order.module.ts
+++ src/main/webapp/app/entities/order/order.module.ts
@@ -5,10 +5,11 @@ import { OrderDetailComponent } from './detail/order-detail.component';
 import { OrderUpdateComponent } from './update/order-update.component';
 import { OrderDeleteDialogComponent } from './delete/order-delete-dialog.component';
 import { OrderRoutingModule } from './route/order-routing.module';
+import { OrderLineComponent } from '../order-line/list/order-line.component';
 
 @NgModule({
   imports: [SharedModule, OrderRoutingModule],
-  declarations: [OrderComponent, OrderDetailComponent, OrderUpdateComponent, OrderDeleteDialogComponent],
+  declarations: [OrderComponent, OrderDetailComponent, OrderUpdateComponent, OrderDeleteDialogComponent, OrderLineComponent],
   entryComponents: [OrderDeleteDialogComponent],
 })
 export class OrderModule {}
