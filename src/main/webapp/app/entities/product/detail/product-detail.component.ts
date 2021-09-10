e6b96aa06cac90e6485619329b854d1c9070ddf5
Aurélien <aurelien.chosson@gmai.com>
2021-09-10 09:19:02 +0200

    HTML refait en partie sur liste produit

--- src/main/webapp/app/entities/product/detail/product-detail.component.ts
+++ src/main/webapp/app/entities/product/detail/product-detail.component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -11,28 +13,39 @@ import { IProduct } from '../product.model';
   templateUrl: './product-detail.component.html',
 })
 export class ProductDetailComponent implements OnInit {
-  product: IProduct | null = null;
+  product = {
+    id: 1,
+    name: 'Bleu',
+    price: 25000,
+    year: 25,
+    region: 'Poitou-Charante',
+    type: 'Rouge',
+    description: 'Il pue',
+    alcoholPer: 50,
+    recommandation: 'Poulet',
+    ageLimit: 10,
+    temperature: 6,
+    stock: 2,
+    urlImage: '../../../content/images/wine-logo.jpg',
+  };
+
   productAdded = false;
 
   constructor(protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ product }) => {
+    /*this.activatedRoute.data.subscribe(({ product }) => {
       this.product = product;
-    });
+    });*/
   }
 
-  previousState(): void {
-    window.history.back();
-  }
-
-  public addToCart(product: IProduct): void {
-    console.log('Product added to cart : ' + product.name);
+  public addToCart(product?: IProduct): void {
+    console.log('Product added to cart : ' + product?.name);
     this.productAdded = true;
   }
 
-  public removeFromCart(product: IProduct): void {
-    console.log('Product removed from cart : ' + product.name);
+  public removeFromCart(product?: IProduct): void {
+    console.log('Product removed from cart : ' + product?.name);
     this.productAdded = false;
   }
 }
